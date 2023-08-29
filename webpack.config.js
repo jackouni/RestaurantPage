789380a1a244232757817742f9e49d9069eb09b9
Jack Sebben <jacksebben11@hotmail.com>
2023-08-29 08:16:06 -0400

    Webpack has been set up and configured.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ["style.loader", "css-loader", "sass-loader"],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Restuarant App",
+			filename: "index.html",
+			template: "src/template.html",
+		}),
+	],
+};
