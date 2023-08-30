6a80ff45c179fe7d238b431ba776f9c4031f8d73
Jack Sebben <jacksebben11@hotmail.com>
2023-08-30 08:29:35 -0400

    Re-formatted the contentCardComponent() to be more dynamic, allowing us to now generate a content-card with sections and subtitles for those sections. This is made like this because this will be a common theme/design pattern that is used for each page of our site. Now we can recycle this function for every page we create. The function takes a parameter of names you want for each section and then generates the sections with those names as sub-titles for the sections, one-by-one in order.
    Function, whoAreWeContent(), now adds content to the 'Who Are We' section of the homepage.

--- src/index.js
+++ src/index.js
@@ -18,34 +18,64 @@ content.append(headerComponent("Home", "Menu", "About"));
 
 // <---------------> //
 
-// Creates a content-card with multiple sections
-function contentCardComponent(sectionsAmount) {
+// Creates a content-card with multiple sections, with sub-titles
+function contentCardComponent(...sectionNames) {
+	// Creates Content-Card:
 	const contentCard = document.createElement("div");
 	contentCard.id = "content-card";
 
-	for (let i = 0; i < sectionsAmount; i++) {
+	for (let i = 0; i < sectionNames.length; i++) {
+		// Creates Section:
 		let section = document.createElement("section");
+		section.id = sectionNames[i].split(" ").join("");
+
+		// Creates Subtitle for Section:
+		let subTitle = document.createElement("h2");
+		subTitle.classList.add("sub-title");
+		subTitle.innerText = sectionNames[i];
+		section.append(subTitle);
+
 		contentCard.append(section);
 	}
+
 	return contentCard;
 }
 
 // <---------------> //
 
-//Home Page
+// <-----HOME PAGE-----> //
 
-content.append(contentCardComponent(4));
+content.append(
+	contentCardComponent("Rusty-Runts", "Who Are We?", "Hours", "Directions")
+);
 
-// Adds the Subtitles to each section of content-card
-function appendSectionTitles(...sectionNames) {
-	const allSections = document.getElementById("content-card").children;
+// Who Are We Section:
+const whoAreWeSection = document.getElementById("WhoAreWe?");
 
-	for (let i = 0; i < allSections.length; i++) {
-		let subTitle = document.createElement("h2");
-		subTitle.classList.add("sub-title");
-		subTitle.innerText = sectionNames[i];
-		allSections[i].append(subTitle);
-	}
+function whoAreWeContent() {
+	let paragraph = document.createElement("p");
+	paragraph.innerText =
+		"Welcome to Rusty-Runts! Here we serve up the best home cookin' in all of Ontario. After 15 years in the business of serving up breakfast, we can assure you the our: world-famous pancakes, waffles and pastries ; top rated, 5 star hospitality ; and vibes will not dissapoint.";
+
+	return paragraph;
 }
 
-appendSectionTitles("Rusty-Runts", "Who Are We?", "Hours", "Directions");
+whoAreWeSection.append(whoAreWeContent());
+
+// Hours Section:
+const hoursSection = document.getElementById("Hours");
+
+let hours = {
+	weekdays: {
+		days: ["Monday", "Tuesday", "Wednesday", "Thursday"],
+		hours: "9:30AM - 8PM",
+	},
+	fridays: {
+		days: ["Friday"],
+		hours: "9:30AM - 1:30PM",
+	},
+	weekends: {
+		days: ["Saturday", "Sunday"],
+		hours: "Closed",
+	},
+};
